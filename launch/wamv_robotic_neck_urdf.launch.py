2800ae110d4131ea028e08406a42f19723661d41
MonkyDCristian <cristian.nova@uc.cl>
2023-12-10 18:19:22 -0300

    update

--- launch/wamv_robotic_neck_urdf.launch.py
+++ launch/wamv_robotic_neck_urdf.launch.py
@@ -9,9 +9,9 @@ from launch_utils.utils import launch_joint_state_publisher_node as launch_jsp_n
 def generate_launch_description():
     launchs = []
     
-    launchs.append(launch_rsp_node(package_name="robotic_neck_viz", xacro_file="robot.urdf.xacro"))
+    launchs.append(launch_rsp_node(package_name="robotic_neck_viz", xacro_file="wamv_robotic_neck.urdf.xacro"))
     launchs.append(launch_jsp_node(gui=True))
     launchs.append(launch_rviz_node(package_name='robotic_neck_viz', config_file='wamv.rviz'))
-    launchs.append(Node(package='robotic_neck_viz',executable='laser_sim', name='laser_sim'))
-    
+    launchs.append(include_launch(package_name="robotic_neck_driver", launch_file="camera.launch.py"))
+
     return LaunchDescription(launchs)
